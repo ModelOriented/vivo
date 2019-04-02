02a563332bbc049402a953325f35ea9d065e9b13
Anna Kozak <anna1993kozak@gmail.com>
2019-04-02 14:19:59 +0200

    change a theme in plot

--- R/plot_local_importance.R
+++ R/plot_local_importance.R
@@ -4,6 +4,7 @@
 #'
 #' @param measure object returned from LocalVariableImportanceViaOscillations
 #'
+#' @return a ggplot2 object
 #' @examples
 #' \dontrun{
 #' measure <- LocalVariableImportanceViaOscillations(cp, df, absolute_deviation = TRUE, point = TRUE, density = FALSE, kernel_density = "gaussian", bw_density = "nrd0")
@@ -15,6 +16,10 @@
 
 plot.local_importance <- function(measure){
   df <- as.data.frame(measure)
-  ggplot2::ggplot(df, aes(x = factor(df$variable_name , levels = df$variable_name[order(-df$measure)]), y = df$measure)) + geom_bar(stat = "identity") +
-    xlab("_x_") + ylab("measure") + ggtitle("")
+  ggplot(df, aes(x = factor(df$variable_name , levels = df$variable_name[order(-df$measure)]), ymin = 0, ymax = df$measure)) +
+    geom_errorbar() +
+    xlab("_x_") +
+    ylab("measure") +
+    ggtitle("") +
+    theme_mi2()
 }
