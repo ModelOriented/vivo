2f003bcbf5c3748f90af2af04cf41f99344ed2be
kozaka93 <anna1993kozak@gmail.com>
2019-05-19 22:34:20 +0200

    adding test

--- tests/testthat/test_measure.R
+++ tests/testthat/test_measure.R
@@ -2,6 +2,39 @@ context("Check LocalVariableImportance() function")
 
 source("objects_for_tests.R")
 
+test_that("local_variable_importance", {
+  expect_true(is.data.frame(LocalVariableImportance(cp, apartments[, 2:5], absolute_deviation = TRUE, point = TRUE, density = TRUE)))
+})
+
+test_that("local_variable_importance", {
+  expect_true(is.data.frame(LocalVariableImportance(cp, apartments[, 2:5], absolute_deviation = TRUE, point = TRUE, density = FALSE)))
+})
+
+test_that("local_variable_importance", {
+  expect_true(is.data.frame(LocalVariableImportance(cp, apartments[, 2:5], absolute_deviation = TRUE, point = FALSE, density = TRUE)))
+})
+
+test_that("local_variable_importance", {
+  expect_true(is.data.frame(LocalVariableImportance(cp, apartments[, 2:5], absolute_deviation = FALSE, point = TRUE, density = TRUE)))
+})
+
+test_that("local_variable_importance", {
+  expect_true(is.data.frame(LocalVariableImportance(cp, apartments[, 2:5], absolute_deviation = FALSE, point = TRUE, density = FALSE)))
+})
+
+test_that("local_variable_importance", {
+  expect_true(is.data.frame(LocalVariableImportance(cp, apartments[, 2:5], absolute_deviation = TRUE, point = FALSE, density = FALSE)))
+})
+
+test_that("local_variable_importance", {
+  expect_true(is.data.frame(LocalVariableImportance(cp, apartments[, 2:5], absolute_deviation = FALSE, point = FALSE, density = TRUE)))
+})
+
+test_that("local_variable_importance", {
+  expect_true(is.data.frame(LocalVariableImportance(cp, apartments[, 2:5], absolute_deviation = FALSE, point = FALSE, density = FALSE)))
+})
+
+
 test_that("local_variable_importance_data.frame", {
   expect_error(LocalVariableImportance(cp, as.matrix(apartments[, 2:5])))
 })
@@ -19,5 +52,5 @@ test_that("calculate_weight_list", {
 })
 
 test_that("calculate_weight_data.frame", {
-  expect_error(CalculateWeight(cp, as.matrix(apartments[, 2:5]), split))
+  expect_error(CalculateWeight(explainer_rf, as.matrix(apartments[, 2:5]), split))
 })
